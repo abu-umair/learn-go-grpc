7d08f88f3783eedc7b49a29b16932f17abbe1fd5
abu-umair <mrdwnw93@gmail.com>
2025-06-20 15:28:02 +0700

    tambah memanggil library

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -10,5 +12,12 @@ func main() {
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal("There is error in your net listen ", err)
+	}
+
+	serv := grpc.NewServer()
+
+	if err := serv.Serve(lis); err != nil {
+		// log.Fatal(v...: "Error running server ", err)
+		log.Fatal("Error running server ", err)
 	}
 }
