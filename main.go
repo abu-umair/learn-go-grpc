51caf645540055be3a47ef66d841937a9cffb439
abu-umair <mrdwnw93@gmail.com>
2025-06-20 15:26:51 +0700

    tambah listen

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	// lis, err := net.Listen(network: "tcp", address: ":8080") //ditutorial seperti ini
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("There is error in your net listen ", err)
+	}
 }
