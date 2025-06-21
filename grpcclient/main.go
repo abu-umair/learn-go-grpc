23e5bb93db6c50aac653a0511869f34f93ef2fda
abu-umair <mrdwnw93@gmail.com>
2025-06-21 11:33:09 +0700

    tambah membuat client connection

--- grpcclient/main.go
+++ grpcclient/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	grpc.NewClient("localhost:8081", grpc.WithTransportCredentials(insecure.NewCredentials())) //insecure.NewCredentials(): jika dilingkungan develop
+	clientConn, err := grpc.NewClient("localhost:8081", grpc.WithTransportCredentials(insecure.NewCredentials())) //insecure.NewCredentials(): jika dilingkungan develop
+	if err != nil{
+		log.Fatal("Failed create client", err)
+	}
 }
